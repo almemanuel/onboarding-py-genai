762a6c3a7938a2186eccdacd56c0d50c981726ea
emanuel <emanuel.almeida1998@outlook.com>
2026-03-14 00:50:45 -0300

    feat(src): tools

--- src/__main__.py
+++ src/__main__.py
@@ -3,11 +3,19 @@ from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.chat_history import InMemoryChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain.agents import create_agent
+from langchain_core.tools import tool
 import os
 
 
 load_dotenv()
 history_store = {}
+
+@tool
+def get_repo_info(repo_name: str) -> str: # parameter type is important for the model to understand what kind of input it should provide when calling the tool
+    """Retorna informações de um repositório.""" # model read the docstring to understand what the tool does
+    # mock por enquanto
+    return f"Repositório {repo_name}: 42 stars, linguagem Python, última atualização hoje."
 
 def get_session(session_id: str) -> InMemoryChatMessageHistory:
     if session_id not in history_store:
@@ -17,6 +25,12 @@ def get_session(session_id: str) -> InMemoryChatMessageHistory:
 llm = ChatGroq(
     model="llama-3.3-70b-versatile",
     api_key=os.getenv("GROQ_API_KEY")
+)
+
+agent = create_agent(
+    model=llm,
+    tools=[get_repo_info],
+    system_prompt="Você é um assistente técnico."
 )
 
 reviewer_prompt = ChatPromptTemplate.from_messages([
@@ -48,22 +62,31 @@ chain_with_memory = RunnableWithMessageHistory(
 # print("---")
 
 # primeira chamada
-res = chain_with_memory.invoke(
-    {
-        "input": "print('hello world')"
-    },
-    config={"configurable": {"session_id": "emanuel"}}
-)
-print(res.content)
+# res = chain_with_memory.invoke(
+#     {
+#         "input": "print('hello world')"
+#     },
+#     config={"configurable": {"session_id": "emanuel"}}
+# )
+# print(res.content)
 
-print("---")
-print("HISTÓRICO:", history_store["emanuel"].messages)
-print("---")
-# segunda chamada
-res = chain_with_memory.invoke(
-    {
-        "input": "o que você achou do código anterior?"
-    },
-    config={"configurable": {"session_id": "emanuel"}}
-)
-print(res.content)
+# print("---")
+# print("HISTÓRICO:", history_store["emanuel"].messages)
+# print("---")
+# # segunda chamada
+# res = chain_with_memory.invoke(
+#     {
+#         "input": "o que você achou do código anterior?"
+#     },
+#     config={"configurable": {"session_id": "emanuel"}}
+# )
+# print(res.content)
+
+res = agent.invoke({"messages": [{"role": "user", "content": "me fala sobre o repositório langchain"}]})
+print(res["messages"][-1].content)
+
+# intern raw response with tool calls and all
+for msg in res["messages"]:
+    # print(msg)
+    print(type(msg).__name__, ":", msg.content) # more legible output
+    print("---")
